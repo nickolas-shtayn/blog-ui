8d1168b686d04ef39809ae588900e83601d00fbf
Nickolas Shtayn <nickolasshtayn@protonmail.com>
2025-05-08 20:28:24 -0500

    Adjust code length to 5 chars

--- invite/script.js
+++ invite/script.js
@@ -10,7 +10,7 @@ function generateRandomString() {
   const characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let result = "";
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < 5; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
